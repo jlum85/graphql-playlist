dba1a91e6d1c3a118464fec23979b0e88a89a7e7
jlum85 <jlum@wanadoo.fr>
2020-04-28 21:59:26 +0200

    BookType

--- server/app.js
+++ server/app.js
@@ -1,7 +1,21 @@
-const express = require('express');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
 
 const app = express();
 
+// bind express with graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    // pass in a schema property
+  })
+);
+
+app.get("/", function (req, res) {
+  console.log("get / ");
+  return res.send("hello / ");
+});
+
 app.listen(4000, () => {
-    console.log('now listening for requests on port 4000');
+  console.log("now listening for requests on port 4000");
 });
