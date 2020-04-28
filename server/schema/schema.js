4f85d130bd70bfec51501d8f882989be13721696
jlum85 <jlum@wanadoo.fr>
2020-04-28 22:38:40 +0200

    RootQuery

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+var books = [
+  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -9,4 +17,25 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } }, // pour pouvoir faire une requête du type :  book(id: '1') { name, genre}
+      resolve(parent, args) {
+        // code to get data from  db
+        return _.find(books, { id: args.id });
+        // const res = books.filter((item) => item.id === args.id)[0];
+        // console.log(res);
+        // return res;
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
