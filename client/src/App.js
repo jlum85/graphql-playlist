19c1269f940633f320baf8af618144fb50624690
jlum85 <jlum@wanadoo.fr>
2020-04-30 20:37:14 +0200

    ApolloClient

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-function App() {
-  return (
-    <div id="main">
-      <h1>Ninja reading list</h1>
-    </div>
-  );
+// components
+import BookList from "./components/BookList";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
