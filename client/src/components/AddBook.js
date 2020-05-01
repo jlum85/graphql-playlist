005f3a263757fd09a5695024fe7f63631b03aa9d
jlum85 <jlum@wanadoo.fr>
2020-05-01 21:51:47 +0200

    external querry file

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
-import { gql } from "apollo-boost";
 import { graphql } from "react-apollo"; // pour binder la requ^te
 
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`;
+import { getAuthorsQuery } from "../queries/queries";
 
 class AddBook extends Component {
   displayAuthors() {
